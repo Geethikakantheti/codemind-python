e44e4e3366f9e6f7ae7b3ea36e3cb62972f7602a
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2022-10-29 14:26:53 +0530

    N=int(input()) l=list(map(int,input().split())) j=0 for i in range(len(l)):     if(l[i]==0):         l[i],l[j]=l[j],l[i]         j+=1 for i in range(N):     print(l[i],end=" ")

---
+++ Sort_an_Array_of_0s_and_1s.py
@@ -0,0 +1,9 @@
+N=int(input())
+l=list(map(int,input().split()))
+j=0
+for i in range(len(l)):
+    if(l[i]==0):
+        l[i],l[j]=l[j],l[i]
+        j+=1
+for i in range(N):
+    print(l[i],end=" ")
